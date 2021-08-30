9c29fd733f1f8c04cbc51a9e39067b77e4d715f3
averak <abe12@mccc.jp>
2021-08-30 17:29:24 +0900

    [feat] create stamp migration files

---
+++ app/src/main/resources/db/migration/V0_0_3__insert_user_role.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user_role` (`id`, `name`) VALUES
+(1, "ADMIN"),
+(2, "MEMBER");
---
+++ app/src/main/resources/db/migration/V0_1_2__create_stamp_attachment.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `stamp_attachment` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `stamp_id` INT UNSIGNED NOT NULL,
+  `uuid` CHAR(36) NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  INDEX `fk_stamp_id_idx` (`stamp_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_stamp_id`
+    FOREIGN KEY (`stamp_id`)
+    REFERENCES `stamp` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
