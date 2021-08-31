f94581aa72b853ec35406465d9ee0fba6f204eba
averak <abe12@mccc.jp>
2021-08-31 20:34:00 +0900

    [feat] hello angular!!

---
+++ app/src/main/client/src/app/enums/user-role-enum.ts
@@ -0,0 +1,4 @@
+export enum UserRoleEnum {
+  ADMIN = 1,
+  MEMBER = 2,
+}
---
+++ app/src/main/client/src/app/model/user-model.ts
@@ -0,0 +1,22 @@
+import { UserRoleEnum } from '../enums/user-role-enum';
+
+// ユーザモデル
+export interface UserModel {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  roleId: UserRoleEnum;
+}
+
+// ユーザ一覧モデル
+export interface UsersModel {
+  users: UserModel[];
+}
+
+// アクセストークンモデル
+export interface AccessTokenModel {
+  accessToken: string;
+  tokenType: string;
+}
